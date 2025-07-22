c8a7da9311bba113cee71bf5a2cad9da5bb7c808
Roisland <3254546276@qq.com>
2025-07-22 12:23:47 +0800

    finish hw0b

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,5 +1,4 @@
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
@@ -7,7 +6,14 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> map = new TreeMap<>();
+        String wordlist = "abcdefghijklmnopqrstuvwxyz";
+        int map_num = 1;
+        for (char c: wordlist.toCharArray()) {
+            map.put(c, map_num);
+            map_num++;
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +21,34 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> square = new HashMap<>();
+        for (Integer num: nums) {
+            square.put(num, (int)Math.pow(num, 2));
+        }
+        return square;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        int num_count;
+        List<String> lst = new ArrayList<>();
+        Map<String, Integer> map = new TreeMap<>();
+        for (String n : words) {
+            lst.add(n);
+        }
+        while(!lst.isEmpty()) {
+            num_count = 0;
+            String temp = lst.getFirst();
+            for (String n : lst) {
+                if (n.equals(temp)) {
+                    num_count++;
+                }
+            }
+            map.put(temp, num_count);
+            for (int i = 0; i < map.get(temp); i++) {
+                lst.remove(temp);
+            }
+        }
+        return map;
     }
 }
