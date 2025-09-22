f6b92c398c1179ed3b7d85b4ba2982ce502ebff3
Roisland <3254546276@qq.com>
2025-09-22 21:08:56 +0800

    finish proj1b

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,143 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+
+public class ArrayDeque61B<T> implements  Deque61B<T> {
+
+    private final int INITIAL_CAP = 8;
+    private T[] items;
+    private int size;
+    private int start;
+    private int NextFirst;
+    private int NextLast;
+
+
+
+    public ArrayDeque61B() {
+        this.size = 0;
+        this.items = (T[]) new Object[INITIAL_CAP];
+        this.start = 0;
+        this.NextLast = Math.floorMod(start + 1, INITIAL_CAP);
+        this.NextFirst = Math.floorMod(start - 1, INITIAL_CAP);
+    }
+
+    public void ResizingDown() {
+        T[] NewItems = (T[]) new Object[items.length / 2];
+        for (int i = 0; i < size; i++) {
+            NewItems[i] = this.get(i);
+        }
+        this.NextFirst = Math.floorMod(start - 1, NewItems.length);
+        this.NextLast = Math.floorMod(start + size, NewItems.length);
+        items = NewItems;
+    }
+
+    public void ResizingUp() {
+        T[] NewItewms = (T[]) new Object[2 * items.length];
+        for(int i = 0; i < size; i++) {
+            NewItewms[i] = this.get(i);
+        }
+        this.NextFirst = Math.floorMod(start - 1, NewItewms.length);
+        this.NextLast = Math.floorMod(start + size, NewItewms.length);
+        items = NewItewms;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (size == 0) {
+            items[start] = x;
+        } else {
+            if (size >= items.length) {
+                this.ResizingUp();
+            }
+            items[NextFirst] = x;
+            NextFirst = Math.floorMod(NextFirst - 1, items.length);
+        }
+
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size == 0) {
+            items[start] = x;
+        } else {
+            if (size >= items.length) {
+                this.ResizingUp();
+            }
+            items[NextLast] = x;
+            NextLast = Math.floorMod(NextLast + 1, items.length);
+        }
+
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnlist = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            returnlist.add(this.get(i));
+        }
+        return returnlist;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public T removeFirst() {
+        T returntemp = null;
+        if (this.isEmpty()) {
+            return returntemp;
+        } else {
+            if (size <= items.length / 4) {
+                this.ResizingDown();
+            }
+            NextFirst = Math.floorMod(NextFirst + 1, items.length);
+            returntemp = items[NextFirst];
+        }
+        size--;
+        return  returntemp;
+    }
+
+    @Override
+    public T removeLast() {
+        T returntemp = null;
+        if (this.isEmpty()) {
+            return returntemp;
+        } else {
+            if (size <= items.length / 4) {
+                this.ResizingDown();
+            }
+            NextLast = Math.floorMod(NextLast - 1, items.length);
+            returntemp = items[NextLast];
+        }
+        size--;
+        return  returntemp;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        } else {
+            return items[Math.floorMod(NextFirst + 1 + index, items.length)];
+        }
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,9 +1,4 @@
-import jh61b.utils.Reflection;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Field;
-import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
@@ -19,5 +14,53 @@ public class ArrayDeque61BTest {
 //
 //         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
 //     }
+
+    @Test
+    public void AddFirsthandAddLastandRemoveTest() {
+        Deque61B<Integer> D1 = new ArrayDeque61B<>();
+        D1.addFirst(1);//[1]
+        assertThat(D1.toList()).containsExactly(1).inOrder();
+        D1.addLast(3);//[1, 3]
+        assertThat(D1.toList()).containsExactly(1, 3).inOrder();
+        D1.addFirst(2);//[2, 1, 3]
+        assertThat(D1.toList()).containsExactly(2, 1, 3).inOrder();
+        D1.addFirst(5);//[5, 2, 1, 3]
+        D1.addFirst(6);//[6, 5, 2, 1, 3]
+        D1.addFirst(2);//[2, 6, 5, 2, 1, 3]
+        D1.addLast(9);//[2, 6, 5, 2, 1, 3, 9]
+        D1.addLast(9);//[2, 6, 5, 2, 1, 3, 9, 9]
+        assertThat(D1.toList()).containsExactly(2, 6, 5, 2, 1, 3, 9, 9).inOrder();
+        D1.addFirst(3);//[3, 2, 6, 5, 2, 1, 3, 9, 9]
+        assertThat(D1.toList()).containsExactly(3, 2, 6, 5, 2, 1, 3, 9, 9).inOrder();
+        assertThat(D1.removeFirst()).isEqualTo(3);//[2, 6, 5, 2, 1, 3, 9, 9]
+        assertThat(D1.removeFirst()).isEqualTo(2);//[6, 5, 2, 1, 3, 9, 9]
+        assertThat(D1.removeLast()).isEqualTo(9);//[6, 5, 2, 1, 3, 9]
+        assertThat(D1.removeLast()).isEqualTo(9);//[6, 5, 2, 1, 3]
+        assertThat(D1.removeLast()).isEqualTo(3);//[6, 5, 2, 1]
+        assertThat(D1.toList()).containsExactly(6, 5, 2, 1).inOrder();
+        assertThat(D1.removeLast()).isEqualTo(1);//[6, 5, 2]
+        assertThat(D1.toList()).containsExactly(6, 5, 2).inOrder();
+    }
+
+    @Test
+    public void GetTest() {
+        Deque61B<Integer> D1 = new ArrayDeque61B<>();
+        D1.addFirst(1);//[1]
+        D1.addFirst(2);//[2, 1]
+        D1.addLast(3);//[2, 1, 3]
+        assertThat(D1.get(0)).isEqualTo(2);
+        assertThat(D1.get(1)).isEqualTo(1);
+        assertThat(D1.get(2)).isEqualTo(3);
+    }
+
+    @Test
+    public  void IsEmptyTest() {
+        Deque61B<Integer> D1 = new ArrayDeque61B<>();
+        assertThat(D1.isEmpty()).isTrue();
+        D1.addFirst(1);
+        assertThat(D1.isEmpty()).isFalse();
+    }
+
+
 
 }
